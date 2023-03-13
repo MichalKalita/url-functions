4ef318fd3e964c53a31c5f2815cce717f8643265
Michal Kalita <kalita.michal@gmail.com>
2023-03-13 23:27:55 +0100

    add svelte kit demo

--- packages/example-svelte-kit/src/routes/+page.svelte
+++ packages/example-svelte-kit/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { updateUrl } from 'create-url';
+
+	$: url = updateUrl($page.url);
+	$: number = Number($page.url.searchParams.get('number')) || 0;
+	$: array = $page.url.searchParams.getAll('a') || [];
+</script>
+
+<h1>SvelteKit usage example</h1>
+
+<a href={url({ pathname: '', hash: '', deleteQuery: true })}>Reset all parameters</a>
+
+<div>
+	<h2 id="query">Query</h2>
+	<a href="#query">native link with hash</a>
+	<a href={url({ hash: 'query' })}>Generated link with hash</a>
+	<p>Number: {number}</p>
+	<a href={url({ query: { number: (n) => Number(n || 0) + 1 } })}>Increment</a>
+	<a href={url({ query: { number: (n) => Number(n || 0) - 1 } })}>Decrement</a>
+</div>
+
+<div>
+	<h2 id="query-array">Update array in query</h2>
+	<p>Array: [{array.join(', ')}]</p>
+	<a href={url({ query: { a: [...array, Number(array.at(-1) || 0) + 1] } })}>Add item to list</a>
+	<a href={url({ query: { a: array.slice(1) } })}>Remove first</a>
+	<a href={url({ query: { a: array.slice(0, -1) } })}>Remove last</a>
+</div>
---
+++ packages/example-svelte-kit/src/routes/[...path]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	import { updateUrl } from 'create-url';
+
+	$: url = updateUrl($page.url);
+
+	$: lastPath = $page.url.pathname.split('/').pop() || '';
+	$: nextPath = String.fromCharCode(lastPath.charCodeAt(lastPath.length - 1) + 1);
+</script>
+
+<h1>
+	Page '{$page.params.path}'
+</h1>
+
+<p>Page with [...path] in router</p>
+
+<a href={url({ path: '..' })}>Go level up and keep all parameters</a>
+<a href={url({ path: nextPath })}>Go to next page and keep all parameters</a>
