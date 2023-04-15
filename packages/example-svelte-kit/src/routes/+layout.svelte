9bcae7fd511da2d97f458ce531600bc58deb0c32
Michal Kalita <kalita.michal@gmail.com>
2023-04-15 18:27:03 +0200

    better styles for demo

--- packages/example-svelte-kit/src/routes/+layout.svelte
+++ packages/example-svelte-kit/src/routes/+layout.svelte
@@ -5,36 +5,121 @@
 	$: url = updateUrl($page.url);
 	$: number = Number($page.url.searchParams.get('number')) || 0;
 	$: array = $page.url.searchParams.getAll('a') || [];
+	$: urlQueryObject = Array.from($page.url.searchParams.entries()).reduce((acc, [key, value]) => {
+		if (key in acc) {
+			if (Array.isArray(acc[key])) {
+				acc[key].push(value);
+			} else {
+				acc[key] = [acc[key], value];
+			}
+		} else {
+			acc[key] = value;
+		}
+
+		return acc;
+	}, {});
 </script>
 
-<h1>SvelteKit usage example</h1>
+<div class="note">Layout <span>+layout.svelte</span></div>
+<div class="box">
+	<h1>Examples, how to use with link tools</h1>
+	<div class="actions">
+		<a href="#query">native link with hash</a>
+		<a href={url({ hash: 'query' })}>Generated link with hash</a>
 
-<a href={url({ clearPreviousQuery: true })}>Reset all queries and hash</a>
-<a href={url({ clearQuery: true })}>Reset all queries and hash</a>
-<a href={url({ clear: true })}>Reset all queries and hash</a>
+		<a href={url({ clearPreviousQuery: true })}>Reset all queries and hash</a>
+		<a href={url({ clearQuery: true })}>Reset all queries and hash</a>
+		<a href={url({ clear: true })}>Reset all queries and hash</a>
 
-<a href={url({ keepQuery: false })}>Reset all queries and hash</a>
+		<a href={url({ keepQuery: false })}>Reset all queries and hash</a>
 
-<a href={url({ extendQuery: false })}>Reset all queries and hash</a>
-<a href={url({ deleteQuery: true })}>Reset all queries and hash</a>
+		<a href={url({ extendQuery: false })}>Reset all queries and hash</a>
+		<a href={url({ deleteQuery: true })}>Reset all queries and hash</a>
 
-<a href={url({ rewriteQuery: true, query: { key: 'value' } })}>Rewrite query</a>
+		<a href={url({ rewriteQuery: true, query: { key: 'value' } })}>Rewrite query</a>
+	</div>
 
-<div>
-	<h2 id="query">Query</h2>
-	<a href="#query">native link with hash</a>
-	<a href={url({ hash: 'query' })}>Generated link with hash</a>
-	<p>Number: {number}</p>
-	<a href={url({ query: { number: (n) => Number(n || 0) + 1 } })}>Increment</a>
-	<a href={url({ query: { number: (n) => Number(n || 0) - 1 } })}>Decrement</a>
+	<div class="note mt-5">Page parameters <span>{$page.route.id}</span></div>
+	<div class="box">
+		<pre>{JSON.stringify($page.params, null, 2)}</pre>
+	</div>
+
+	<div class="note mt-5">URL Query</div>
+	<div class="box">
+		<pre>{JSON.stringify(urlQueryObject, null, 2)}</pre>
+
+		<div class="note mt-5">Number <span>{number}</span></div>
+		<div class="box actions">
+			<a href={url({ query: { number: (n) => Number(n || 0) + 1 } })}>
+				Increment number <span>{'url({ query: { number: (n) => Number(n || 0) + 1 } })'}</span>
+			</a>
+			<a href={url({ query: { number: (n) => Number(n || 0) - 1 } })}>
+				Decrement <span>{'url({ query: { number: (n) => Number(n || 0) - 1 } })'}</span>
+			</a>
+		</div>
+
+		<div class="note mt-5">Array <span>[{array.join(', ')}]</span></div>
+		<div class="box actions">
+			<a href={url({ query: { a: [...array, Number(array.at(-1) || 0) + 1] } })}>
+				Add item to list <span
+					>{'url({ query: { a: [...array, Number(array.at(-1) || 0) + 1] } })'}</span
+				>
+			</a>
+			<a href={url({ query: { a: array.slice(1) } })}>
+				Remove first <span>{'url({ query: { a: array.slice(1) } })'}</span>
+			</a>
+			<a href={url({ query: { a: array.slice(0, -1) } })}>
+				Remove last <span>{'url({ query: { a: array.slice(0, -1) } })'}</span>
+			</a>
+		</div>
+	</div>
+
+	<div class="note mt-5">Page in path <span>{$page.route.id}</span></div>
+	<div class="box">
+		<slot />
+	</div>
 </div>
 
-<div>
-	<h2 id="query-array">Update array in query</h2>
-	<p>Array: [{array.join(', ')}]</p>
-	<a href={url({ query: { a: [...array, Number(array.at(-1) || 0) + 1] } })}>Add item to list</a>
-	<a href={url({ query: { a: array.slice(1) } })}>Remove first</a>
-	<a href={url({ query: { a: array.slice(0, -1) } })}>Remove last</a>
-</div>
+<style>
+	.box {
+		border: 1px solid black;
+		padding: 5px;
+	}
 
-<slot />
+	.note {
+		background: black;
+		color: white;
+		display: inline-block;
+		padding: 2px 5px;
+	}
+
+	.note span {
+		font-family: monospace;
+		color: rgb(240, 214, 66);
+	}
+
+	.mt-5 {
+		margin-top: 5px;
+	}
+
+	pre {
+		margin: 0;
+	}
+
+	a {
+		text-decoration: none;
+	}
+	a:hover,
+	a:focus {
+		text-decoration: underline;
+	}
+	a span {
+		font-family: monospace;
+		color: rgb(230, 2, 2);
+	}
+
+	.actions {
+		display: flex;
+		flex-direction: column;
+	}
+</style>
---
+++ packages/example-svelte-kit/src/routes/lang/[[lang]]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: lang = $page.params.lang;
+</script>
+
+<h1>Example for language switching</h1>
+
+Actual language is {lang}.
